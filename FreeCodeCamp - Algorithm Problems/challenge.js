240c51aa67c4ebea9bb4e51a38347bf855a91d42
Jess Taylor <jessicatee84@gmail.com>
2018-02-19 18:23:36 +0000

    Completed check for palindrome

--- FreeCodeCamp - Algorithm Problems/challenge.js
+++ FreeCodeCamp - Algorithm Problems/challenge.js
@@ -1,4 +1,22 @@
+// reverse a string challenge
 const reverseString = (str) => str.split('').reverse().join('');
 
-console.log(reverseString("hello"));
-console.log(reverseString("yey, i remebered arrow syntax"));
+// console.log(reverseString("hello"));
+// console.log(reverseString("yey, i remebered arrow syntax"));
+
+// check for palindrome after removing non-alphanumeric characters incl '_'
+function palindrome(str) {
+ 	let charactersRemoved = str.replace(/\W|_/g, '');
+
+ 	if (str.replace(/\W|_/g, '').toLowerCase() == charactersRemoved.toLowerCase().split('').reverse().join(''))
+ 	{
+ 		return true;
+ 	} else {
+ 		return false;
+ 	}
+}
+
+//console.log(palindrome("My age is 0, 0 si ega ym."));
+//console.log(palindrome("race car"));
+
+
