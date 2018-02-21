0760264a4c88cb9ea7b455c15c7f21b1b7cdb7e0
Jess Taylor <jessicatee84@gmail.com>
2018-02-21 16:00:21 +0000

    Completed Return largest numbers in array & Confirm the ending challenges

--- FreeCodeCamp - Algorithm Problems/challenge.js
+++ FreeCodeCamp - Algorithm Problems/challenge.js
@@ -1,17 +1,25 @@
-// Free code camp Algorithm Challenges
-// 	1 - Reverse a string
-// 	2 - Check for palindromes 
-//	3 - Find longest word in a string
-// 	4 - Title case a sentence
+/*
+  Free code camp Basic Algorithm Challenges
+
+	1 - Reverse a string
+       - Reverse the provided string.
+	2 - Check for palindromes 
+       - Return true if the given string is a palindrome. Otherwise, return false.
+	3 - Find longest word in a string
+       - Return the length of the longest word in the provided sentence.
+	4 - Title case a sentence
+       - Return the provided string with the first letter of each word capitalized. Make sure the rest of the word is in lower case.
+ 5 - Return largest numbers in array
+       - Return an array consisting of the largest number from each provided sub-array.
+ 6 - Confirm the ending
+       - Check if a string (first argument, str) ends with the given target string (second argument, target).
+*/
 
 
-
-// reverse a string challenge
 const reverseString = (str) => str.split('').reverse().join('');
 // console.log(reverseString("hello"));
 // console.log(reverseString("yey, i remebered arrow syntax"));
 
-// check for palindrome after removing non-alphanumeric characters incl '_'
 function palindrome(str) {
  	let charactersRemoved = str.replace(/\W|_/g, '');
 
@@ -25,7 +33,6 @@ function palindrome(str) {
 //console.log(palindrome("My age is 0, 0 si ega ym."));
 //console.log(palindrome("race car"));
 
-// Return the length of the longest word in the provided sentence.
 function findLongestWord(str) {
   let arr = str.split(' ');
   let longestWord = 0;
@@ -36,11 +43,9 @@ function findLongestWord(str) {
   		longestWord = arr[i].length;
   	}
   }
-
   return longestWord;
 }
 
-// return string with first letter of each word capitalised
 function titleCase(str) {
   let arr = str.split(' ');
   arr.forEach(function(word, i) {
@@ -50,28 +55,26 @@ function titleCase(str) {
   });
   return arr.join(' ');
 }
+//console.log(titleCase("I'm a little tea pot"));
 
-console.log(titleCase("I'm a little tea pot"));
+// Return an array consisting of the largest number from each provided sub-array. 
+function largestOfFour(arr) {
+  let largestNumbers = [];
+  arr.forEach(function(numArray) {
+    let biggestNumber = 0;
+    numArray.forEach(function(num) {
+      if (num > biggestNumber) {
+        biggestNumber = num;
+      }
+    });
+    largestNumbers.push(biggestNumber);
+  });
+  return largestNumbers;
+}
+//console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]));
 
+function confirmEnding(str, target) {
+  return (str.substr(str.length - target.length, target.length) == target);
+}
+// console.log(confirmEnding("Bastian", "n"));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//console.log(findLongestWord("The quick brown fox jumped over the lazy dog"));
